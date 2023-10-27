f509dba876d54682cf36c0a6903c2db5a808b817
Bruna Moura <brunamoura@MacBook-Air-de-Bruna-2.local>
2023-10-27 10:44:12 -0300

    hove

--- monitor.py
+++ monitor.py
@@ -22,7 +22,7 @@ disable_hover_plotly_css = """
 @media (hover: none), (pointer: coarse) {
     /* Desativar hover nos elementos Plotly com a classe 'nsewdrag drag' */
     .nsewdrag.drag {
-        pointer-events: none !important;
+        pointer-events: none !mousemove;
     }
 }
 </style>
