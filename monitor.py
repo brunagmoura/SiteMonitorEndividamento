8c31cf428005853a463abf2477c53ff563b66192
Bruna Moura <brunamoura@MacBook-Air-de-Bruna-2.local>
2023-10-27 20:47:56 -0300

    Desistimos do hover

--- monitor.py
+++ monitor.py
@@ -13,52 +13,29 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 from datetime import datetime as dt, timedelta
+import streamlit.components.v1 as components
 
 warnings.filterwarnings('ignore')
 
 st.set_page_config(page_title="Monitor endividamento", page_icon=":bar_chart:", layout="wide", initial_sidebar_state="collapsed", menu_items={"About": "Link ou descrição aqui"})
 
 
-#disable_hover_plotly_css = """
-#<style>
-#@media (hover: none), (pointer: coarse) {
-#    /* Desativar hover e interação para os elementos Plotly especificados */
-#    nsewdrag drag {
-#        pointer-events: none;
-#    }
-#}
-#</style>
-#"""
+#Desabilitar o hover no celular
 
-# Adicionando o CSS na aplicação
-#st.markdown(disable_hover_plotly_css, unsafe_allow_html=True)
-
-# Script JavaScript para detectar movimento do mouse
-js_script = """
-<script>
-document.addEventListener('DOMContentLoaded', (event) => {
-    document.addEventListener('mousemove', function(e) {
-        let element = document.getElementById('mouse-area');
-        let bounds = element.getBoundingClientRect();
-
-        if (e.pageX > bounds.left && e.pageX < bounds.right && e.pageY > bounds.top && e.pageY < bounds.bottom) {
-            element.innerHTML = 'Movimento do mouse detectado!';
-        }
-    });
-});
-</script>
+disable_hover_plotly_css = """
+<style>
+@media (hover: none), (pointer: coarse) {
+   /* Desativar hover e interação para os elementos Plotly especificados */
+   nsewdrag drag {
+       pointer-events: none;
+   }
+}
+</style>
 """
 
-# Elemento HTML que reage ao movimento do mouse
-html_container = """
-<div id="mouse-area" style="border:1px solid black; height: 100px;">
-    Mova o mouse aqui
-</div>
-"""
+st.markdown(disable_hover_plotly_css, unsafe_allow_html=True)
 
-# Mostrar o container HTML e o script JavaScript
-html(html_container)
-st.markdown(js_script, unsafe_allow_html=True) 
+#Início da página
 
 st.title(" :bar_chart: Monitor do endividamento dos brasileiros")
 
@@ -843,3 +820,4 @@ html = f"""
 """
 
 st.markdown(html, unsafe_allow_html=True)
+
